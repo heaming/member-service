bab16fa13572e09623231818583e9b83d279e64e
heaming <meetoohi@gmail.com>
2024-06-10 17:47:48 +0900

    Initial commit - docker, mysql setting

---
+++ src/main/java/com/kafka/memeberservice/MemeberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.memeberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeberServiceApplication.class, args);
+    }
+
+}
